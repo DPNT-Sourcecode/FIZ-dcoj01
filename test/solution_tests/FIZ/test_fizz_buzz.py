ee5dbfea5bcb694b2c411b92bb2041b06bfc6bdf
sbx_user1051 <sbx_user1051@169.254.153.37>
2019-11-16 21:43:25 +0000

    Sat Nov 16 21:43:25 UTC 2019

--- test/solution_tests/FIZ/test_fizz_buzz.py
+++ test/solution_tests/FIZ/test_fizz_buzz.py
@@ -1,41 +1,41 @@
 from accelerate_runner.lib.solutions.FIZ import fizz_buzz_solution as fb
 
+
 class TestFizz_buzz():
     def test_fizz_buzz(self):
-       assert (fb.fizz_buzz(3) == 'fizz')
-       assert (fb.fizz_buzz(5) == 'buzz')
-       assert (fb.fizz_buzz(15) == 'fizz buzz')
-       assert (fb.fizz_buzz(999) == 'deluxe')
-       assert (fb.fizz_buzz(555) == 'fizz buzz deluxe')
-       assert (fb.fizz_buzz(1) == 1)
-
+        assert (fb.fizz_buzz(3) == 'fizz')
+        assert (fb.fizz_buzz(5) == 'buzz')
+        assert (fb.fizz_buzz(15) == 'fizz buzz')
+        assert (fb.fizz_buzz(999) == 'deluxe')
+        assert (fb.fizz_buzz(555) == 'fizz buzz deluxe')
+        assert (fb.fizz_buzz(1) == 1)
 
     def test_check_fizz_buzz_deluxe(self):
-       assert (fb.check_fizz_buzz_deluxe(555) == True)
-       assert (fb.check_fizz_buzz_deluxe(989) == False)
+        assert (fb.check_fizz_buzz_deluxe(555) == True)
+        assert (fb.check_fizz_buzz_deluxe(989) == False)
 
     def test_check_deluxe(self):
-       assert (fb.check_deluxe(999) == True)
-       assert (fb.check_deluxe(232) == False)
+        assert (fb.check_deluxe(999) == True)
+        assert (fb.check_deluxe(232) == False)
 
     def test_check_fizz_buzz(self):
-       assert (fb.check_fizz_buzz(15) == True)
-       assert (fb.check_fizz_buzz(33) == False)
+        assert (fb.check_fizz_buzz(15) == True)
+        assert (fb.check_fizz_buzz(33) == False)
 
     def test_check_fizz(self):
-       assert (fb.check_fizz(18) == True)
-       assert (fb.check_fizz(35) == False)
+        assert (fb.check_fizz(18) == True)
+        assert (fb.check_fizz(35) == False)
 
     def test_check_buzz(self):
-       assert (fb.check_buzz(10) == True)
-       assert (fb.check_buzz(33) == False)
+        assert (fb.check_buzz(10) == True)
+        assert (fb.check_buzz(33) == False)
 
     def test_check_digit(self):
-       assert (fb.check_digit(999) == True)
-       assert (fb.check_digit(949) == False)
+        assert (fb.check_digit(999) == True)
+        assert (fb.check_digit(949) == False)
 
     def test_check_number(self):
-       assert (fb.check_number(335,3) == True)
-       assert (fb.check_number(335,6) == False)
+        assert (fb.check_number(335, 3) == True)
+        assert (fb.check_number(335, 6) == False)
 
 
