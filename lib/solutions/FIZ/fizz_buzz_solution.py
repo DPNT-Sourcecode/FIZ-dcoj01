30c6bf59cb4410770d3303a6b644a1a366ca7e40
sbx_user1051 <sbx_user1051@169.254.153.37>
2019-11-16 16:37:54 +0000

    Sat Nov 16 16:37:54 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,3 +1,12 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    raise NotImplementedError()
+    if number == range(1, 9999):
+        if number % 3 == 0 and number % 5 == 0:
+            return 'fizz buzz'
+        elif number % 3 == 0:
+            return 'fizz'
+        elif number % 5 == 0:
+            return 'buzz'
+        return number
+
+fizz_buzz(3)
