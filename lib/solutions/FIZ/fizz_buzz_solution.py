a43515631686b2433f89e7221af8913823f40f67
sbx_user1051 <sbx_user1051@169.254.153.37>
2019-11-16 21:27:04 +0000

    Sat Nov 16 21:27:04 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -34,7 +34,7 @@ def check_deluxe(number):
 
 
 def check_fizz_buzz_deluxe(number):
-    return number > 10 and check_digit(number) and check_fizz_buzz_deluxe(number)
+    return check_deluxe(number) and check_fiz_buzz(number)
 
 
 def check_digit(number):
@@ -45,6 +45,7 @@ def check_digit(number):
 
 def check_number(number, digit):
     return str(digit) in str(number)
+
 
 
 
