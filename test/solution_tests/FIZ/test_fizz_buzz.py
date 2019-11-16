ac33a65ff4bbc354d8489a4e52e9b6f56f32304a
sbx_user1051 <sbx_user1051@169.254.153.37>
2019-11-16 17:20:51 +0000

    Sat Nov 16 17:20:51 UTC 2019

--- test/solution_tests/FIZ/test_fizz_buzz.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from accelerate_runner.lib.solutions.FIZ import fizz_buzz_solution as fb
-
-class TestFizz_buzz(TestCase):
-    assert(fb.fizz_buzz(3) == 'fizz')
-
-
-
